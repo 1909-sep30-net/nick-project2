9d60c577999260be9b7ac5ecd2b777f86e717f04
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-11-07 14:59:55 -0600

    add a real test to bring code coverage above 0

---
+++ KitchenRestService/KitchenRestService.Tests/Logic/FridgeServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KitchenRestService.Logic;
+using Moq;
+using Xunit;
+
+namespace KitchenRestService.Tests.Logic
+{
+    public class FridgeServiceTests
+    {
+        [Fact]
+        public async Task CleanFridgeDoesNothingOnEmptyFridge()
+        {
+            var mockRepo = new Mock<IKitchenRepo>();
+            mockRepo.Setup(r => r.GetAllFridgeItemsAsync())
+                .ReturnsAsync(Enumerable.Empty<FridgeItem>());
+
+            var service = new FridgeService(mockRepo.Object);
+
+            var cleaned = await service.CleanFridgeAsync();
+
+            Assert.False(cleaned);
+        }
+    }
+}
