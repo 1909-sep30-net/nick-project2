c9d715e7a6861d8eb1c6313be61866dbfa5003ac
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-11-07 15:28:06 -0600

    well dont revert the part that makes it pass

--- intro-app/e2e/src/app.e2e-spec.ts
+++ intro-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('intro-app app is running!');
+    expect(page.getTitleText()).toEqual('the fridge is running!');
   });
 
   afterEach(async () => {
